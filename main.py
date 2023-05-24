4a51e842a604d900f4843dcbca5b1c6d30f5269f
Axel Rothing <axelrothing@icloud.com>
2023-05-24 20:45:22 +0200

    Final release package

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ from website import create_app
 app = create_app()
 if __name__ == "__main__":
     print("Flask Server Started!")
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
